973a50e21e4fed65f41bdecb37802e9d01d27d96
sabitameem <sabitameem578@gmail.com>
2023-05-07 16:56:16 +0530

    added data from client side to server side

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
-
+const cors =require('cors')
 const port = process.env.PORT || 5000;
-
-const user =[
+app.use(cors())
+app.use(express.json())
+const users =[
     {id:1, name:'john', email:'john@example.com'},
     {id:2, name:'Kabir', email:'kabir@example.com'},
     {id:3, name:'mesha', email:'mesha@example.com'},
@@ -15,7 +16,15 @@ app.get('/', (req,res)=>{
 })
 
 app.get('/users',(req,res)=>{
-    res.send(user)
+    res.send(users)
+})
+
+app.post('/users', (req,res)=>{
+    console.log(req.body)
+    const newUser = req.body;
+    newUser.id=users.length+1;
+    users.push(newUser);
+    res.send(newUser)
 })
 
 app.listen(port,() =>{
